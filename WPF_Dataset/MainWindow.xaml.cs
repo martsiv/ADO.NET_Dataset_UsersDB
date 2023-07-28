fa1c6fc9a52bf72d533261ffeee16c51386c5460
martsiv <mykola.martsiv@gmail.com>
2023-07-28 21:12:19 +0300

    Add project files.

---
+++ WPF_Dataset/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WPF_Dataset
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string connStr = null;
+        private SqlDataAdapter adapter = null;
+        private DataSet dataSet = null;
+        private DataTable mixedTable = null;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            connStr = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+            LoadTable();
+        }
+        private void LoadTable()
+        {
+            string cmd = "select * from Users";
+            adapter = new(cmd, connStr);
+            new SqlCommandBuilder(adapter);
+            adapter.TableMappings.Add("Users", "Users");
+            dataSet = new DataSet();
+            adapter.Fill(dataSet, "Users");
+            grid.ItemsSource = dataSet.Tables[0].DefaultView;
+        }
+        private void ClickSave(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                adapter.Update(dataSet, "Users");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void ClickRefresh(object sender, RoutedEventArgs e)
+        {
+            dataSet = new DataSet();
+            adapter.Fill(dataSet, "Users");
+            grid.ItemsSource = dataSet.Tables[0].DefaultView;
+        }
+
+        //Local filter by roles
+        private void SetFilterByRole(int roleID)
+        {
+            DataViewManager dvm = new DataViewManager(dataSet);
+            dvm.DataViewSettings["Users"].RowFilter = $"RoleID = {roleID}";
+            DataView dataView1 = dvm.CreateDataView(dataSet.Tables["Users"]);
+
+            grid.ItemsSource = null;
+            grid.ItemsSource = dataView1;
+        }
+        private void ClickFilterByAdmins(object sender, RoutedEventArgs e) => SetFilterByRole(1);
+        private void ClickFilterByModerators(object sender, RoutedEventArgs e) => SetFilterByRole(2);
+        private void ClickFilterByUsers(object sender, RoutedEventArgs e) => SetFilterByRole(3);
+
+        //Procedure calls
+        private void ExecuteProcedure(string sp_name)
+        {
+            SqlDataAdapter ad = new(sp_name, connStr);
+            ad.SelectCommand.CommandType = CommandType.StoredProcedure;
+            //arguments possible e.g.
+            //adapt.SelectCommand.Parameters.Add(new SqlParameter("@ChatRoomID", SqlDbType.VarChar, 100));
+            //adapt.SelectCommand.Parameters["@ChatRoomID"].Value = chatroomidno;
+            new SqlCommandBuilder(ad);
+            DataTable dataTable = new();
+            ad.Fill(dataTable);
+        }
+        private void ClickDeleteAdmins(object sender, RoutedEventArgs e) => ExecuteProcedure("sp_delete_admins");
+        private void ClickDeleteModerators(object sender, RoutedEventArgs e) => ExecuteProcedure("sp_delete_moderators");
+        private void ClickDeleteUsers(object sender, RoutedEventArgs e) => ExecuteProcedure("sp_delete_users");
+    }
+}
---
+++ WPF_Dataset/SQLUsersDB.sql
@@ -0,0 +1,58 @@
+﻿create database UsersDB
+go
+use UsersDB
+go
+create table Roles
+(
+	ID int primary key identity,
+	[Role name] nvarchar(50) not null,
+);
+go
+
+create table Users
+(
+	[Login] nvarchar(50) primary key,
+	[Password] varchar(20) not null,
+	[Address] nvarchar(100) not null,
+	[Phone] nvarchar(12) not null CHECK (Phone LIKE '380[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'), 
+	[RoleID] int not null,
+	CHECK ([Password] LIKE '%[0-9]%'
+       AND [Password] LIKE '%[a-z]%'
+       AND [Password] LIKE '%[A-Z]%'
+       AND LEN([Password]) >= 8),
+	FOREIGN KEY ([RoleID]) REFERENCES Roles(ID),
+);
+go
+
+use UsersDB
+go
+insert into [Roles]
+values	('Admin'),
+		('Moderator'),
+		('User');
+go
+insert into [Users]
+([Login], [Password], [Address], [Phone], [RoleID])
+values
+		('Leonard', 'J32234r23I', 'Rivne, Soborna 13', '380958417483', 1),
+		('Delamene', 'k384E385q43', 'Rivne, Drahomanova 25', '380972658190', 1),
+		('Fred', 'He3938tW2l', 'Rivne, Wasylkiwska 17', '380937582943', 2),
+		('John', 'L34895F34476q', 'Rivne, Karnaukhowa 15', '380673859104', 2),
+		('Bernard', '457H43487q42', 'Rivne, Bila 5', '380735618503', 3),
+		('Harry', 'L3434i355398Q', 'Rivne, Dubenska 172', '380983950271', 3);
+go
+
+create or alter proc sp_delete_admins
+as
+delete from Users where RoleID = (select ID from Roles where [Role name] = 'Admin')
+go
+
+create or alter proc sp_delete_moderators
+as
+delete from Users where RoleID = (select ID from Roles where [Role name] = 'Moderator')
+go
+
+create or alter proc sp_delete_users
+as
+delete from Users where RoleID = (select ID from Roles where [Role name] = 'User')
+go
